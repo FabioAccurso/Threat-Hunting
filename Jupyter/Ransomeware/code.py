c7f9b5e7473a5b0c2390395be8aef7920c2c0a73
Fabio Accurso <92177206+FabioAccurso@users.noreply.github.com>
2024-02-16 11:52:44 +0100

    Update code.py

--- Jupyter/Ransomeware/code.py
+++ Jupyter/Ransomeware/code.py
@@ -20,19 +20,21 @@ data['LastModified0x30'] =  pd.to_datetime(data['LastModified0x30'], format='%Y-
 data['LastRecordChange0x10'] =  pd.to_datetime(data['LastRecordChange0x10'], format='%Y-%m-%d %H:%M:%S.%f')
 data['LastRecordChange0x30'] =  pd.to_datetime(data['LastRecordChange0x30'], format='%Y-%m-%d %H:%M:%S.%f')
 
-
+#Analisi 1
 dates = data["LastRecordChange0x10"]
 dates.index = dates.dt.to_period('d')
 s = dates.groupby(level=0).size()
 s.sort_values(ascending=False).head(10)
 
+#Analisi 2
 fig = px.bar(names_plot, x="FileName", "title=Files with more entries")
 Fig.show()
 
-readme = data[data["FileName"].str.contains("_readme_.txt")]
-redme.sort_values(by="LastModified0x10", ascending=True).head()
+#Visualizzazione piu nel dettaglio di Analisi 2
+#readme = data[data["FileName"].str.contains("_readme_.txt")]
+#redme.sort_values(by="LastModified0x10", ascending=True).head()
 
-
+#Analisi 3
 first_note = readme1.sort_values(by="LastModified0x10", ascending=True).iloc[0]["LastModified0x10"]
 range_exe = first_note + pd.offsets.Hour(-12)
 data_filtered = data[(data['Created0x10'] > "2021-05-22") & (data['Created0x10'] < "2021-05-25")]
